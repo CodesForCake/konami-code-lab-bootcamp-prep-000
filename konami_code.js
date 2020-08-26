59b8aacd4767548118bbc68a3ac4daa998e45039
Mimi C <mina123fire@yahoo.com>
2020-08-26 18:27:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -26,16 +26,3 @@ function init() {
     }
   })
 }
-
-function onKeyDownHandler(e){
-  const key=e.key;
-  if (key===codes[index]) {
-    index++;
-    if (index===alphabet.length-1) {
-      alert("Hurray!");
-      index=0;
-    }
-  } else {
-    index=0;
-  }
-}
