1147219881f6dd7db8c828d6422432505d63a206
Mimi C <mina123fire@yahoo.com>
2020-08-26 18:25:28 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,8 +11,21 @@ const codes = [
   "a"
 ];
 
+let index=0;
+
 function init() {
-  document.body.addEventListener('keydown',function(event){
-    const alphabet=['a','b']
-  })
+  document.body.addEventListener('keydown',onKeyDownHandler)
+}
+
+function onKeyDownHandler(e){
+  const key=e.key;
+  if (key===codes[index]) {
+    index++;
+    if (index===alphabet.length) {
+      alert("Hurray!");
+      index=0;
+    }
+  } else {
+    index=0;
+  }
 }
